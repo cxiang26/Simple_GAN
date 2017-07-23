08e74720c90aeaf24f3ea3d5512e18f5dc06b4e3
Shaun <cq.xiang@foxmail.com>
2017-07-23 09:46:41 +0800

    Update upgrading.m

--- upgrading.m
+++ upgrading.m
@@ -7,4 +7,16 @@ net.vb = net.vb - net.lr * sum(net.d_vb,2)/net.batch_size;
 
 net.mw = net.lr * net.d_w/net.batch_size;
 net.mv = net.lr * net.d_v/net.batch_size;
+
+%% WGAN 对参数进行了clip
+% a = (net.w > 0.1) * 0.1;
+% b = (a < -0.1) * -0.1;
+% c = (net.w => -0.1) .* (net.w <= 0.1) .* net.w;
+% net.w = a + b + c;
+
+% a = (net.v > 0.1) * 0.1;
+% b = (a < -0.1) * -0.1;
+% c = (net.v => -0.1) .* (net.v <= 0.1) .* net.v;
+% net.v = a + b + c;
+
 end
